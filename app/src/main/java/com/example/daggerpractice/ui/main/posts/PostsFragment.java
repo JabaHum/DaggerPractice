47a8fc042cbd7653040d503accb1937957978c6b
JabaHum <jabajnan8@gmail.com>
2020-06-19 23:57:47 +0300

    Implemented Posts Fragment

--- app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsFragment.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/posts/PostsFragment.java
@@ -44,7 +44,7 @@ public class PostsFragment extends DaggerFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_posts, container, false);
     }
 
 
